489b7c3bc2331731f43154440e2bc6e806b30cc3
Matthew Moss <mdm@cse.unsw.edu.au>
2013-11-02 14:30:49 +1100

    Initial, broken implementation of [RIBJ 2013]: 1

---
+++ iterative-filter.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+import math
+
+"""
+Implementation of Algorithm 1 from [RIBJ 2013]
+
+Input:
+    X: Readings of each sensor
+    N: Number of sensors
+    T: Number of readings for each sensor
+
+Output: Reputation vector r
+
+Note:
+    x(a,b): reading of sensor a at time b
+    X contains readings of each sensor, indexed by time interval
+"""
+
+# Test data
+intel_X = [[19.3612, 19.3612, 19.3612],
+           [19.42, 19.4102, 19.42],
+           [19.0084, 19.0084, 19.0084],
+           [18.5674, 18.5478, 17.117],
+           [17.95, 21.282, 21.3408],
+           [22.153, 21.347, 20.813],
+           [18.0088, 18.0088, 21.625],
+           [20.4, 20.4098, 19.7924]]
+intel_N = 8
+intel_T = 3
+
+def aggregate(instant_readings, weights):
+    # dot product
+    top = sum([r * w for r, w in zip(instant_readings, weights)])
+    bottom = sum(weights)
+    return round(top / bottom, 4)
+
+assert(aggregate([x[0] for x in intel_X], [1] * intel_N) == 19.3586)
+assert(aggregate([x[1] for x in intel_X], [1] * intel_N) == 19.6719)
+assert(aggregate([x[2] for x in intel_X], [1] * intel_N) == 19.8097)
+
+def compute_next_r(readings, weights):
+    # matrix rotation
+    instant_readings = [[x[i] for x in readings] for i in range(len(readings[0]))]
+    return [aggregate(r, weights) for r in instant_readings]
+
+assert(compute_next_r(intel_X, [1] * intel_N) == [19.3586, 19.6719, 19.8097])
+
+def sensor_distance(sensor_readings, next_r):
+    eucl_norm = math.sqrt(sum([(x - r)**2 for x, r in zip(sensor_readings, next_r)]))
+    return eucl_norm
+
+assert(sensor_distance([1,2,3], [1,2,3]) == 0)
+assert(sensor_distance([1,1,1,1], [0,0,0,0]) == 2)
+
+def compute_d(readings, next_r):
+    return [sensor_distance(x, next_r) / len(readings[0]) for x in readings]
+
+#assert(compute_d([[1,1,1,1]], [0,0,0,0]) == [1])
+assert(compute_d([[1,1,1,1]], [1,1,1,1]) == [0])
+
+def discriminant(distance, k):
+    return distance**(-k)
+
+def compute_next_w(distances, readings, next_r):
+    return [discriminant(distances[i], sensor_distance(readings[i], next_r)) for i in range(len(distances))]
+
+def iterative_filter(x, n, t):
+    l = 0
+    w = [[1] * n]
+    r = [[]]
+    converged = False
+    while not converged:
+        print ('round {} ===================='.format(l))
+        
+        r.append(compute_next_r(x, w[l]))
+        print ('r[{}]: {}'.format(l+1, r[l+1]))
+        d = compute_d(x, r[l+1])
+        w.append(compute_next_w(d, x, r[l+1]))
+        print ('w[{}]: {}'.format(l+1, [round(x, 4) for x in w[l+1]]))
+
+        l += 1;
+        if (r[l] == r[l-1]):
+            converged = True
+    return r[l]
+
+if __name__ == '__main__':
+    with open('datasets/intel-temp.csv') as f:
+        raw = [l.rstrip().split(',') for l in f]
+        readings = [[float(r) for r in l] for l in raw]
+        
+        print ('X:')
+        for line in readings:
+            print (line)
+        print ('N: {}'.format(len(readings)))
+        print ('T: {}'.format(len(readings[0])))
+        print ()
+    
+        print(iterative_filter(readings, len(readings), len(readings[0])))
