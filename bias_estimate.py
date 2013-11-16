648427530881eda5a649b56f832e2d8edd622f39
Matthew Moss <mdm@cse.unsw.edu.au>
2013-11-16 18:13:36 +1100

    Some kind of working bias estimation

--- bias_estimate.py
+++ bias_estimate.py
@@ -1,11 +1,39 @@
 #!/usr/bin/python
 
+from scipy.optimize import minimize
+
 # Readings format: Array of sensors containing array of times
 # [[S1T1, S1T2], [S2T1, S2T2]]
 
-def delta(i, j, readings):
-    assert(len(readings[i]) == len(readings[j]))
-    return sum([a - b for (a, b) in zip(readings[i], readings[j])]) / len(readings[i])
+# Danger Will Robinson: Lack of thread safety ahead!
+_readings = []
 
-assert(delta(0, 1, [[0, 0], [0, 0]]) == 0)
-assert(delta(0, 1, [[1, 1], [0, 0]]) == 1)
+def delta(i, j):
+    assert(len(_readings[i]) == len(_readings[j]))
+    return sum([a - b for (a, b) in zip(_readings[i], _readings[j])]) / len(_readings[i])
+
+def per_sensor_bias(i, bi, j, bj):
+    return (bi - bj - delta(i, j))**2
+
+def bias_estimator(x):
+    total = 0
+    for i in range(len(x)):
+        for j in range(i):
+            total += per_sensor_bias(i, x[i], j, x[j]) 
+    return total
+
+def bias_constraint(x):
+    return (sum(x) == 0)
+
+bias_constraint = {
+    'type': 'eq',
+    'fun': bias_constraint,
+}
+
+def estimate(readings):
+    global _readings
+    _readings = readings 
+    res = minimize(bias_estimator, [0]*len(readings), method='SLSQP', constraints=bias_constraint, options={'disp':True})
+    return res.x
+
+print (estimate([[1,0], [0, 1]]))
