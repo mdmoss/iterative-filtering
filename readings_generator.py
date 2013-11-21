894a062419f0a184a9c7bd7c049ddb2b48cd2100
Edward Pierzchalski <e.a.pierzchalski@gmail.com>
2013-11-21 11:39:40 +1100

    finished variance estimator and testing.

--- readings_generator.py
+++ readings_generator.py
@@ -8,9 +8,10 @@ def readings(biases, variances, num_times, true_value):
     num_sensors = len(biases)
     bias_compensator = -mean(biases)
     compensated_biases = biases + bias_compensator * array([1] * len(biases))
+    standard_deviations = [v**0.5 for v in variances]
     return array([
         [random.gauss(true_value(t) + compensated_biases[sensor],
-                      variances[sensor])
+                      standard_deviations[sensor])
          for t in range(num_times)]
         for sensor in range(num_sensors)
     ])
--- variance_estimate.py
+++ variance_estimate.py
@@ -15,7 +15,7 @@ def variance_estimate(readings, biases):
     num_sensors = len(readings)
     num_times = len(readings[0])
     #debiased_readings[s, t] is \hat{x}^t_s
-    debiased_readings = array(readings) - array([biases[sensor] * num_times for sensor in range(num_sensors)])
+    debiased_readings = array(readings) - array([[biases[sensor]] * num_times for sensor in range(num_sensors)])
 
     def beta_matrix_entry(sensor1, sensor2):
         return sum([
@@ -60,27 +60,37 @@ def variance_estimate(readings, biases):
 if __name__ == "__main__":
     num_sensors = 20
     biases = array([0.3 * sensor for sensor in range(num_sensors)])
+    compensated_biases = biases - array([mean(biases)] * num_sensors)
     variances = array([(num_sensors - sensor + 1) / 2 for sensor in range(num_sensors)])
     num_times = 10
     true_value = lambda t: (t - num_times / 2) ** 2
     num_readings = 20
-    reading_sampling = [readings_generator.readings(biases, variances, num_times, true_value) for i in
+    reading_sampling = [readings_generator.readings(compensated_biases, variances, num_times, true_value) for i in
                         range(num_readings)]
-    bias_estimates = [bias_estimate(r) for r in reading_sampling]
+    bias_estimates = array([bias_estimate(r) for r in reading_sampling])
     variance_estimates = array([variance_estimate(r, b) for r, b in zip(reading_sampling, bias_estimates)])
     alpha = 0.95
     #variance_estimates[i,s] gives the estimate of sensor s in reading i.
     #variance_estimates.transpose()[s, i] gives the same.
-    confidence_intervals = [stats.bayes_mvs(v_est, alpha) for v_est in variance_estimates.transpose()]
+    variance_cis = [stats.bayes_mvs(v_est, alpha) for v_est in variance_estimates.transpose()]
+    variance_mean_cis = [m for m, v, s in variance_cis]
+    variance_mean_cntrs = [cntr for cntr, bounds in variance_mean_cis]
+    variance_mean_bounds = [bounds for cntr, bounds in variance_mean_cis]
 
-    compensated_biases = biases - array([mean(biases)] * num_sensors)
+    bias_cis = [stats.bayes_mvs(b_est, alpha) for b_est in bias_estimates.transpose()]
+    bias_mean_cis = [m for m, v, s in bias_cis]
+    bias_mean_cntrs = [cntr for cntr, bounds in bias_mean_cis]
+
     fig = pyplot.figure()
     axes = fig.add_subplot(1, 1, 1)
     axes.set_title('Variance Estimation')
     axes.plot(variances)
+    axes.plot(variance_mean_cntrs)
+    #axes.plot(compensated_biases)
+    #axes.plot(bias_mean_cntrs)
 
     #axes.legend(['True', 'Estimated - Mean', 'Estimated - Standard Deviation'])
-    axes.set_ylim(ymax=3)
+    #axes.set_ylim(ymax=3)
     axes.set_xlabel('Sensor ID')
     axes.set_ylabel('Bias')
 
