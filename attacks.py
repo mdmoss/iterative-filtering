aab88746edd11b71f581edfcfb4b056bcd337741
Edward Pierzchalski <e.a.pierzchalski@gmail.com>
2013-11-23 20:24:49 +1100

    moved attack generators to their own file

--- attacks.py
+++ attacks.py
@@ -1,22 +1,53 @@
-from numpy import array, vstack
+from numpy import array, vstack, mean, concatenate
 from readings_generator import readings
-from iterative_filter import iterfilter, exponential
+import robust_aggregate
 
 __author__ = 'Edward'
 
 
-def readings_simple_attack(legitimate_readings, num_colluders, colluder_bias):
-    aggregate = iterfilter(legitimate_readings, exponential)
-    collusion_readings = [[v + colluder_bias for v in aggregate]] * num_colluders
-    return vstack((array(legitimate_readings), array(collusion_readings)))
+def transpose(l):
+    return map(list, zip(*l))
 
 
-def readings_sophisticated_attack(legitimate_readings, num_colluders, colluder_bias):
-    aggregate = iterfilter(legitimate_readings, exponential)
-    collusion_readings = [[v + colluder_bias for v in aggregate]] * (num_colluders - 1)
-    all_readings = vstack((array(legitimate_readings), array(collusion_readings)))
-    final_colluder_readings = iterfilter(all_readings, exponential)
-    return vstack((all_readings, array(final_colluder_readings)))
+def readings_simple_attack(legitimate_readings,
+                           true_value,
+                           num_colluders, colluder_bias):
+    num_times = len(legitimate_readings[0])
+    colluder_values = [[true_value(num_times + t) + colluder_bias for t in range(num_times)]
+                       for i in range(num_colluders)]
+    return array(legitimate_readings.tolist() + colluder_values)
+
+
+def readings_sophisticated_attack(legitimate_readings,
+                                  true_value,
+                                  num_colluders, colluder_bias):
+    num_times = len(legitimate_readings[0])
+    simple_colluder_values = [[true_value(num_times + t) + colluder_bias for t in range(num_times)]
+                              for i in range(num_colluders - 1)]
+    legitimate_and_simple_readings = legitimate_readings.tolist() + simple_colluder_values
+    averaging_colluder_values = [mean(t_readings) for t_readings in transpose(legitimate_and_simple_readings)]
+    return array(legitimate_and_simple_readings + [averaging_colluder_values])
+
+
+def readings_ks_attack(legitimate_readings,
+                       true_value,
+                       num_colluders, colluder_bias):
+    """
+        readings[s,t] is reading of sensor s at time t
+
+    """
+
+    def estimate(readings):
+        return robust_aggregate.estimate(readings, robust_aggregate.exponential)
+
+    b = colluder_bias / (num_colluders - 1)
+    colluder_value = [v + b for v in estimate(legitimate_readings)]
+    readings_with_colluders = vstack((array(legitimate_readings), array(colluder_value)))
+    for i in range(num_colluders - 2):
+        colluder_value = [v + b for v in estimate(readings_with_colluders)]
+        readings_with_colluders = vstack((array(readings_with_colluders), array(colluder_value)))
+    final_colluder_value = estimate(readings_with_colluders)
+    return vstack((array(readings_with_colluders), array(final_colluder_value)))
 
 
 if __name__ == "__main__":
@@ -25,8 +56,13 @@ if __name__ == "__main__":
     colluder_bias = 6
     biases = [0] * num_legitimate_sensors
     variances = [1] * num_legitimate_sensors
-    num_times = 8
+    num_times = 4
     true_value = lambda t: 0
     legitimate_readings = readings(biases, variances, num_times, true_value)
-    simple_attack_readings = readings_sophisticated_attack(legitimate_readings, num_colluders, colluder_bias)
+    simple_attack_readings = readings_simple_attack(legitimate_readings, true_value, num_colluders, colluder_bias)
+    sophisticated_attack_readings = readings_sophisticated_attack(legitimate_readings, true_value, num_colluders,
+                                                                  colluder_bias)
+    sk_attack_readings = readings_ks_attack(legitimate_readings, true_value, num_colluders, colluder_bias)
     print(simple_attack_readings)
+    print(sophisticated_attack_readings)
+    print(sk_attack_readings)
--- ks_attack.py
+++ ks_attack.py
@@ -1,26 +1,8 @@
-from numpy import vstack, array
+
+from attacks import readings_ks_attack, readings_simple_attack, readings_sophisticated_attack
 import readings_generator
-import robust_aggregate
 
 __author__ = 'Edward'
-
-
-def attack(readings, num_colluders, colluder_target_bias):
-    """
-        readings[s,t] is reading of sensor s at time t
-
-    """
-    def estimate(readings):
-        return robust_aggregate.estimate(readings, robust_aggregate.exponential)
-
-    b = colluder_target_bias/(num_colluders - 1)
-    colluder_value = [v + b for v in estimate(readings)]
-    readings_with_colluders = vstack((array(readings), array(colluder_value)))
-    for i in range(num_colluders - 2):
-        colluder_value = [v + b for v in estimate(readings_with_colluders)]
-        readings_with_colluders = vstack((array(readings_with_colluders), array(colluder_value)))
-    final_colluder_value = estimate(readings_with_colluders)
-    return vstack((array(readings_with_colluders), array(final_colluder_value)))
 
 if __name__ == "__main__":
     num_legit_sensor = 9
@@ -29,8 +11,15 @@ if __name__ == "__main__":
     true_value = lambda t: 0
     biases = [0] * num_legit_sensor
     variances = [1] * num_legit_sensor
-    readings = readings_generator.readings(biases, variances, num_times, true_value)
-    colluder_target_bias = 10
-    s = attack(readings, num_colluders, colluder_target_bias)
+    legitimate_readings = readings_generator.readings(biases, variances, num_times, true_value)
+    colluder_bias = 10
+    simple_attack_readings, sophisticated_attack_readings, ks_attack_readings = [f(legitimate_readings,
+                                                                                   true_value,
+                                                                                   num_colluders,
+                                                                                   colluder_bias)]
+    simple_attack_readings = readings_simple_attack(legitimate_readings,
+                                                    true_value,
+                                                    num_colluders,
+                                                    colluder_bias)
     print(s)
     print(len(s))
