c3894909fde541cd39835e6e8e907c0230cddb37
Edward Pierzchalski <e.a.pierzchalski@gmail.com>
2013-11-25 00:52:36 +1100

    turns out the simple linear solutions work quite nicely.

--- variance_estimate.py
+++ variance_estimate.py
@@ -5,11 +5,12 @@ __author__ = 'Pierzchalski'
 from numpy import array, mean, vstack
 from scipy import optimize, stats
 import readings_generator
-from bias_estimate import bias_estimate
+from bias_estimate import bias_estimate, linear_solution_bias_estimate
 import matplotlib.pyplot as pyplot
 
 
 def variance_estimate(readings, biases):
+    return linear_solution_variance_estimate(readings, biases)
     """
         `readings` is a sensor-major array of sensor/time readings. `readings[s,t]` is the reading of
         sensor `s` at time `t`.
@@ -50,8 +51,8 @@ def variance_estimate(readings, biases):
     ])
 
     def constraint_function(v):
-                                            #the below somehow reduces the bias. Leaving it out until
-                                            #it has some justification
+    #the below somehow reduces the bias. Leaving it out until
+    #it has some justification
         return sum(v) - constraint_value    #*num_sensors/(num_sensors - 1)
 
     solution = optimize.fmin_slsqp(func=target_function,
@@ -61,6 +62,52 @@ def variance_estimate(readings, biases):
     return solution
 
 
+def linear_solution_variance_estimate(readings, biases):
+    num_sensors = len(readings)
+    num_times = len(readings[0])
+    #debiased_readings[s, t] is \hat{x}^t_s
+    debiased_readings = array(readings) - array([[biases[sensor]] * num_times for sensor in range(num_sensors)])
+
+    def beta_matrix_entry(sensor1, sensor2):
+        return sum([
+            (debiased_readings[sensor1, time] - debiased_readings[sensor2, time]) ** 2
+            for time in range(num_times)
+        ]) / (num_times - 1)
+
+    beta_matrix = array([
+        [beta_matrix_entry(sensor1, sensor2) for sensor2 in range(num_sensors)]
+        for sensor1 in range(num_sensors)
+    ])
+
+    #mean_readings[t] is the mean of all sensor readings at time t.
+    mean_readings = array([mean(r) for r in array(readings).transpose()])
+
+    constraint_value = num_sensors / (num_times * (num_sensors - 1)) * sum([
+        sum([
+            (debiased_readings[sensor, time] - mean_readings[time]) ** 2
+            for time in range(num_times)
+        ])
+        for sensor in range(num_sensors)
+    ])
+
+    target_vector = [sum([beta_matrix[k, i] for i in range(num_sensors)]) for k in range(num_sensors)] + [
+        constraint_value]
+    def diagonal(i, j):
+        if i == j:
+            return num_sensors + 1
+        else:
+            return 1
+    matrix = [
+        [
+            diagonal(i, j)
+            for j in range(num_sensors)
+        ] + [1]
+        for i in range(num_sensors)
+    ] + [[1] * num_sensors + [0]]
+
+    return solve(matrix, target_vector)[:num_sensors]
+
+
 if __name__ == "__main__":
     num_sensors = 20
     biases = array([0.3 * sensor for sensor in range(num_sensors)])
@@ -68,11 +115,11 @@ if __name__ == "__main__":
     variances = array([(num_sensors - sensor + 1) / 2 for sensor in range(num_sensors)])
     num_times = 10
     true_value = lambda t: (t - num_times / 2) ** 4
-    num_readings = 50
+    num_readings = 500
     reading_sampling = [readings_generator.readings(compensated_biases, variances, num_times, true_value) for i in
                         range(num_readings)]
-    bias_estimates = array([bias_estimate(r) for r in reading_sampling])
-    variance_estimates = array([variance_estimate(r, b) for r, b in zip(reading_sampling, bias_estimates)])
+    bias_estimates = array([linear_solution_bias_estimate(r) for r in reading_sampling])
+    variance_estimates = array([linear_solution_variance_estimate(r, b) for r, b in zip(reading_sampling, bias_estimates)])
     alpha = 0.95
     #variance_estimates[i,s] gives the estimate of sensor s in reading i.
     #variance_estimates.transpose()[s, i] gives the same.
@@ -92,6 +139,6 @@ if __name__ == "__main__":
     axes.legend(['True', 'Estimated - Mean'])
     #axes.set_ylim(ymax=3)
     axes.set_xlabel('Sensor ID')
-    axes.set_ylabel('Variance')
+    axes.set_ylabel('Bias')
 
     pyplot.show()
